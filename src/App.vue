51f8a05582c0c64f20b5947493cc2c83e9391bb3
ykhade <ykhade@nevada.unr.edu>
2020-04-11 23:33:53 -0700

    Added markup and sytling

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,131 @@
 <template>
     <div id="app">
-        <h3>Firebase Data</h3>
-        {{firebaseData}}
-        <h3>Form Data</h3>
-        {{firebaseData}}
+        <div class="notification is-success" v-if="state === 'synced'">
+            Form is synced with Firestore
+        </div>
+        <div class="notification is-link" v-else-if="state === 'modified'">
+            From data changed, will sync with Firebase
+        </div>
+        <div class="notification is-warning" v-else-if="state === 'revoked'">
+            From data and Firebase revoked to original data
+        </div>
+        <div class="notification is-danger" v-else-if="state === 'error'">
+            Failed to save to Firestore. {{ errorMessage }}
+        </div>
+        <div class="notification is-info" v-else-if="state === 'loading'">Loading...</div>
 
+        <hr/>
+        <div class="columns">
+            <!-- <div class="column">
+              <h3>Original Data</h3>
+              <br />
+              {{ originalData }}
+            </div> -->
+        </div>
+        <div class="columns">
+            <div class="column">
+                <h3>Vue Form Data</h3>
+                <br/>
+                {{ formData }}
+            </div>
+            <div class="column">
+                <h3>Firestore Data</h3>
+                <br/>
+                {{ firebaseData }}
+            </div>
+        </div>
 
-        <form @submit.prevent="updateFirebase">
-            <input type="text" name="name" v-model="formData.name"/>
-            <input type="email" name="name" v-model="formData.email"/>
-            <input type="tel" name="name" v-model="formData.phone"/>
+        <hr/>
 
-            <button type="submit">Submit</button>
+        <form @submit.prevent="updateFirebase" @input="fieldUpdate">
+            <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                    <label class="label">Contact</label>
+                </div>
+                <div class="field-body">
+                    <div class="field">
+                        <p class="control is-expanded has-icons-left">
+                            <input
+                                    class="input"
+                                    type="text"
+                                    name="name"
+                                    v-model="formData.name"
+
+                            />
+                            <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+              </span>
+                        </p>
+                    </div>
+                    <div class="field">
+                        <p class="control is-expanded has-icons-left has-icons-right">
+                            <input
+                                    class="input"
+                                    type="email"
+                                    name="email"
+                                    v-model="formData.email"
+
+                            />
+                            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                    <label class="label">Phone</label>
+                </div>
+                <div class="field-body">
+                    <div class="field is-expanded">
+                        <div class="field has-addons">
+                            <p class="control">
+                                <a class="button is-static">+1</a>
+                            </p>
+                            <p class="control is-expanded">
+                                <input
+                                        class="input"
+                                        type="tel"
+                                        name="phone"
+                                        v-model="formData.phone"
+
+                                />
+                            </p>
+                        </div>
+                        <p class="help">Do not enter the first zero</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            Vue Form State: {{ state == '' ? 'waiting for changes' : state }}
+            <hr/>
+
+            <button class="button is-success" type="submit" v-if="state === 'modified'">Save Changes</button>
         </form>
+        <button class="button is-warning is-rounded" @click="revokeData">
+      <span class="icon">
+        <i class="fas fa-undo"></i>
+      </span>
+            <span>Revoke to original data</span>
+        </button>
     </div>
 </template>
 
 <script>
     import {db} from "./firebase";
+     import {debounce} from 'debounce';
+     // import Vue from 'vue';
 
     const documentPath = 'contacts/yash'
     export default {
         data() {
             return {
                 firebaseData: null,
+                originalData: null,
                 formData: {},
-                state: 'loading'
+                state: 'loading',
+                errorMessage: ''
             };
         },
 
@@ -44,7 +144,21 @@
                     this.errorMessage = JSON.stringify(error)
                     this.state = 'error'
                 }
-            }
+            },
+            revokeData() {
+                this.form = {...this.originalData}
+                this.updateFirebase()
+                this.state = 'revoked'
+            },
+
+            fieldUpdate() {
+                this.state = 'modified'
+                this.debouncedUpdate()
+            },
+
+            debouncedUpdate: debounce(function () {
+                this.updateFirebase()
+            }, 1500)
         },
 
         created: async function () {
@@ -56,10 +170,28 @@
                 data = {name: '', phone: '', email: ''}
                 docRef.set(data)
             }
-
+            this.originalData = {...data}
             this.formData = data;
             this.state = 'synced'
         },
     };
 </script>
 
+
+<style>
+    h1,
+    h2,
+    h3 {
+        font-weight: 600;
+    }
+
+    #app {
+        font-family: 'sofia-pro', sans-serif;
+        text-align: center;
+        padding: 10vh 15vw;
+    }
+
+    p.help {
+        text-align: left;
+    }
+</style>
