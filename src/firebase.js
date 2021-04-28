5c082000b1fbc4a079cafcb8663265a3b94d9bc0
ykhade <ykhade@nevada.unr.edu>
2021-04-27 22:25:07 -0700

    removing dependabot pushes causing excessive emails

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/firestore'
 
 const firebaseApp = firebase.initializeApp({
     //firebase configuration
-
+    apiKey: "AIzaSyDpR4vBIdmKdumA8GNMpCak8WhLmqZ3h2o",
+    authDomain: "autosave-form.firebaseapp.com",
+    databaseURL: "https://autosave-form.firebaseio.com",
+    projectId: "autosave-form",
+    storageBucket: "autosave-form.appspot.com",
+    messagingSenderId: "679039347135",
+    appId: "1:679039347135:web:801c8d258d0b454849590a",
+    measurementId: "G-WH815WYB9M"
 })
 
 const db = firebaseApp.firestore();
